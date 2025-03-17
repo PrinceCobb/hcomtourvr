8fd90f80ddad73796541b5553dee1935a34b72ca
PrinceCobb <157530780+PrinceCobb@users.noreply.github.com>
2025-03-17 06:40:46 -0400

    intro google link test.js

--- script.js
+++ script.js
@@ -51,7 +51,7 @@ const videoIdsMap = {
 
 const videoUrlsMap = {
   [SCENES.INTRO]:
-    "https://m2qall8jajdg-hls-push.5centscdn.com/mp4/HCOMVideos/intro_fn2.mp4",
+    "https://drive.google.com/file/d/1FQhnoEqsAHdVXmLHN5gitT-kT2UWsmyZ",
   [SCENES.OMM_LAB]:
     "https://m2qall8jajdg-hls-push.5centscdn.com/mp4/comp/OMMFinal.mp4",
   [SCENES.ANATOMY_LAB]:
